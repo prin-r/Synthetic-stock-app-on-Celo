b058b83bada7f15fbf2247fb88cfa3904c32785f
prin-r <q@bandprotocol.com>
2020-05-13 15:57:53 +0700

    -

--- contracts/MockBridge.sol
+++ contracts/MockBridge.sol
@@ -5,14 +5,19 @@ import {IBridge} from "./IBridge.sol";
 
 
 contract MockBridge is IBridge {
-    string public encodedPrice = "3200000000000000";
+    // default is 5000 (50 * 100 (price * multiplier))
+    string public encodedPrice = "8813000000000000";
 
     function setPriceToBe50() public {
-        encodedPrice = "3200000000000000";
+        // price * multiplier
+        // 50 * 100
+        encodedPrice = "8813000000000000";
     }
 
     function setPriceToBe100() public {
-        encodedPrice = "6400000000000000";
+        // price * multiplier
+        // 100 * 100
+        encodedPrice = "1027000000000000";
     }
 
     function relayAndVerify(bytes calldata _data)
@@ -22,7 +27,10 @@ contract MockBridge is IBridge {
         RequestPacket memory req;
         ResponsePacket memory res;
 
+        // oraclescript for stock price
         req.oracleScriptId = 12;
+
+        // strcut {symbol: "^GSPC", multiplier: 100}
         req.params = "050000005e475350436400000000000000";
 
         res.result = encodedPrice;
