59160563c43f98731e50293125b39c9034f6a5d0
prin-r <q@bandprotocol.com>
2020-05-13 18:35:51 +0700

    -

--- celo_interaction/index.js
+++ celo_interaction/index.js
@@ -5,7 +5,12 @@ const kit = Kit.newKit("https://alfajores-forno.celo-testnet.org");
 
 const deployBridgeData = require("./tx_data/deployBridge.js");
 const deployCDPData = require("./tx_data/deployCDP.js");
-const relayAndVerifyData = require("./tx_data/relayAndVerify");
+const approveCDP = require("./tx_data/approveCDP.js");
+const lockCollateral = require("./tx_data/lockCollateral.js");
+const borrow = require("./tx_data/borrow.js");
+const returnDebt = require("./tx_data/returnDebt.js");
+const unlockCollateral = require("./tx_data/unlockCollateral.js");
+const liquidate = require("./tx_data/liquidate.js");
 
 const deployBridgeObj = {
   from: "0x498968C2B945Ac37b78414f66167b0786E522636",
@@ -21,11 +26,51 @@ const deployCDPObj = {
   data: deployCDPData,
 };
 
-const relayAndVerifyObj = {
+const approveCDPObj = {
   from: "0x498968C2B945Ac37b78414f66167b0786E522636",
-  to: "0x0A2acDeA5d23e45d2b5919d5aBdF7CA1088d907b",
-  // Calldata of relayAndVerify function which is copy from Metamask transaction data.
-  data: relayAndVerifyData,
+  // Address of Celo dollar in Alfajores
+  to: "0xa561131a1C8aC25925FB848bCa45A74aF61e5A38",
+  data: approveCDP,
+  gas: "8000000",
+};
+
+const lockCollateralObj = {
+  from: "0x498968C2B945Ac37b78414f66167b0786E522636",
+  // Address of Celo dollar in Alfajores
+  to: "0x3ffBc08b878D489fec0c80fa65C9B3933B361764",
+  data: lockCollateral,
+  gas: "8000000",
+};
+
+const borrowObj = {
+  from: "0x498968C2B945Ac37b78414f66167b0786E522636",
+  // Address of Celo dollar in Alfajores
+  to: "0x3ffBc08b878D489fec0c80fa65C9B3933B361764",
+  data: borrow,
+  gas: "8000000",
+};
+
+const returnDebtObj = {
+  from: "0x498968C2B945Ac37b78414f66167b0786E522636",
+  // Address of Celo dollar in Alfajores
+  to: "0x3ffBc08b878D489fec0c80fa65C9B3933B361764",
+  data: returnDebt,
+  gas: "8000000",
+};
+
+const unlockCollateralObj = {
+  from: "0x498968C2B945Ac37b78414f66167b0786E522636",
+  // Address of Celo dollar in Alfajores
+  to: "0x3ffBc08b878D489fec0c80fa65C9B3933B361764",
+  data: unlockCollateral,
+  gas: "8000000",
+};
+
+const liquidateObj = {
+  from: "0x498968C2B945Ac37b78414f66167b0786E522636",
+  // Address of Celo dollar in Alfajores
+  to: "0x3ffBc08b878D489fec0c80fa65C9B3933B361764",
+  data: liquidate,
   gas: "8000000",
 };
 
@@ -35,9 +80,7 @@ async function awaitWrapper() {
   );
   // This account must have a cGLD balance to pay transaction fees.
 
-  let tx = await kit.sendTransaction(deployBridgeObj);
-
-  // let tx = await kit.sendTransaction(relayAndVerifyObj);
+  let tx = await kit.sendTransaction(unlockCollateralObj);
 
   const receipt = await tx.waitReceipt();
   console.log(receipt);
