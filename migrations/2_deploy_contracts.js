0f3499c1b4b1ebe0a7ae94207670c58547e6cb63
prin-r <q@bandprotocol.com>
2020-05-13 14:38:14 +0700

    -

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 const StockCDP = artifacts.require("StockCDP");
 
 module.exports = function (deployer) {
-  deployer.deploy(StockCDP);
+  deployer.deploy(
+    StockCDP,
+    "0x0000000000000000000000000000000000000000",
+    "0x0000000000000000000000000000000000000000",
+    12,
+    "^GSPC",
+    100,
+  );
 };
